01c80676dd325f9a259a72b2f95fb62965636c5a
Dustin Dortch <Dustin.Dortch@cdw.com>
2021-11-19 12:28:10 -0500

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "rg" {
+  type = object({
+    name     = string
+    location = string
+    tags = object({
+      Owner = string
+    })
+  })
+}
+
+variable "lab_sub_id" {}
+
+variable "stgaccounts" {
+  description = "Data values for storage accounts"
+}
+
+variable "containers" {
+  description = "Data values for storage account containers"
+}
+
+variable "rgs" {
+  description = "Data values for our resource group creation"
+}
+variable "vnets" {
+  description = "Data values for virtual network creation"
+}
+
+variable "peers" {
+  description = "Data values for virtual network peering"
+}
